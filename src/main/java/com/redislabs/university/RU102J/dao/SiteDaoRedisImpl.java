101ad7b0c18f2da6c4072173471b6aade683e4e1
Maxim Kossatyy <max.kossatyy@gmail.com>
2024-03-22 10:50:59 +0500

    w1 change findAll in siteDao

--- src/main/java/com/redislabs/university/RU102J/dao/SiteDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/SiteDaoRedisImpl.java
@@ -42,7 +42,17 @@ public class SiteDaoRedisImpl implements SiteDao {
     @Override
     public Set<Site> findAll() {
         // START Challenge #1
-        return Collections.emptySet();
+        try (Jedis jedis = jedisPool.getResource()) {
+            Set<String> keys = jedis.smembers(RedisSchema.getSiteIDsKey());
+            Set<Site> sites = new HashSet<>();
+            for (String key : keys) {
+                Map<String, String> fields = jedis.hgetAll(key);
+                if (fields != null && !fields.isEmpty()) {
+                    sites.add(new Site(fields));
+                }
+            }
+            return sites;
+        }
         // END Challenge #1
     }
 }
